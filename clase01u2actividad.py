6df36f28851ef45a59729a6caa6ad953c6127a69
guaton4k <jhonquirino14@gmail.com>
2025-09-26 12:38:54 -0300

    CLASE CONTINUA

---
+++ clase01u2actividad.py
@@ -0,0 +1,16 @@
+class Mascotas:
+    def __init__(self, nombre: str, edad: str, especie: str):
+        self._nombre: str = nombre
+        self._edad: int = edad
+        self._especie: str = especie
+
+class Perro:
+    def __init__(self):
+        self.__historial_de_vacunas = []
+
+class Gato:
+    def __init__(self):
+        self.__
+
+class Ave:
+    pass
